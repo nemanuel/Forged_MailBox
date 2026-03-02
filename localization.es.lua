2e7ab6db8f6b7a71a401372c64976ffd71f3553f
Emanuel Nilsson <emanuel.nilsson@gmail.com>
2026-03-02 20:22:06 +0100

    initial commit

---
+++ localization.es.lua
@@ -0,0 +1,12 @@
+Forged_Mailbox = Forged_Mailbox or {}
+local L = Forged_Mailbox.L
+
+if GetLocale() == "esES" then
+  L[ "collected" ]      = "recogido"
+  L[ "1st mail" ]       = "1er mail"
+  L[ "each mail" ]      = "cada mail"
+  L[ "Money received" ] = "Dinero recibido"
+  L[ "All mails" ]      = "Todos los mails"
+  L[ "This mail was returned to you." ] = "Este correo fue devuelto a ti."
+  L[ "This mail was sent by an auctionhouse." ] = "Este correo fue enviado por una casa de subastas."
+end
